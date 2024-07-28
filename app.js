952f8d742c737b3aeb744a190a7073b166241b08
Monika Garg <devmonika4302@gmail.com>
2024-07-28 18:48:23 -0400

    First Discord Bot

---
+++ app.js
@@ -0,0 +1,29 @@
+/* eslint-disable no-undef */
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const { Client, GatewayIntentBits } = require("discord.js");
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+});
+
+client.on("messageCreate", (message) => {
+  if (message.author.bot) return;
+  console.log(message.content);
+  message.reply("Hello from the bot!");
+});
+
+client.on("interactionCreate", async (interaction) => {
+  if (!interaction.isChatInputCommand()) return;
+
+  if (interaction.commandName === "ping") {
+    await interaction.reply("Pong!");
+  }
+});
+
+client.login(process.env.TOKEN);
